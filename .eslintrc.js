c1c80548a702e4f99b1152dcf4dfa0963127ab0b
eric.lin <seekingfor520@gmail.com>
2023-07-02 14:38:52 +0800

    Fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,29 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "prettier"
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
   ],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module"
+    sourceType: 'module',
   },
-  plugins: [
-    "react",
-    "react-hooks",
-    "prettier"
-  ],
+  plugins: ['react', 'react-hooks', 'prettier'],
   rules: {
-    "prettier/ prettier": "error"
+    'prettier/prettier': 'error',
   },
   settings: {
     react: {
-      version: "detect"
-    }
-  }
-}
+      version: 'detect',
+    },
+  },
+};
