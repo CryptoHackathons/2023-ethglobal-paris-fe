55749230e3e613196630ef1a5816ba1ec7013e9e
Han-Hsuan Lin <hhl@utexas.edu>
2023-07-11 00:43:03 +0800

    Feature: form validation - add lottery step 2

--- src/components/add-lottery/AddLotteryStep2.jsx
+++ src/components/add-lottery/AddLotteryStep2.jsx
@@ -1,49 +1,97 @@
-import React, { useState } from 'react';
-import { Button, Accordion } from 'react-bootstrap';
+import React, { useCallback, useState } from 'react';
+import { Button, Accordion, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import AddLotteryRewardSetter from './general/AddLotteryRewardSetter';
+import { useAtom } from 'jotai';
+import { addLotteryAtom } from '../../model/addLottery';
 // import PropTypes from 'prop-types';
+
+const EMPTY_REWARD = {
+  imageURL: '',
+  title: '',
+  description: '',
+  quantity: 0,
+};
 
 export function AddLotteryStep2() {
   const navigate = useNavigate();
-  const emptyReward = {
-    photoPath: '',
-    Title: '',
-    Description: '',
-    RewardAmount: 1,
-  };
-  const [rewards, setRewards] = useState([emptyReward]);
+  const [rewardsDraft, setRewardsDraft] = useAtom(
+    addLotteryAtom.lotteryDraft.rewardsDraft
+  );
 
-  const handleDeleteReward = (id) => {
+  const [validated, setValidated] = useState(false);
+  const [rewards, setRewards] = useState(rewardsDraft.contents);
+
+  const handleDeleteReward = useCallback((id) => {
     setRewards((prev) => {
       return prev.filter((_, index) => index !== id);
     });
-  };
+  }, []);
 
-  const handleAddReward = () => {
+  const handleAddReward = useCallback(() => {
     setRewards((prev) => {
-      return [...prev, emptyReward];
+      return [...prev, { ...EMPTY_REWARD }];
     });
-  };
+    setValidated(false);
+  }, []);
+
+  const handleSubmit = useCallback(
+    (event) => {
+      const form = event.currentTarget;
+
+      event.preventDefault();
+      event.stopPropagation();
+      setValidated(true);
+
+      if (form.checkValidity() === false) return;
+
+      const fd = new FormData(form);
+      const r = rewards.map((r, idx) => ({
+        ...r,
+        imageURL: fd.get(`imageURL.${idx}`),
+        title: fd.get(`title.${idx}`),
+        description: fd.get(`description.${idx}`),
+        quantity: parseInt(fd.get(`quantity.${idx}`)),
+      }));
+
+      console.log('data from rewards form:', r);
+
+      setRewardsDraft((prev) => ({
+        ...prev,
+        totalQuantity: r.reduce((i, acc) => i + acc.quantity, 0),
+        contents: [...r],
+      }));
+      navigate('../missions');
+    },
+    [navigate, rewards, setRewardsDraft]
+  );
 
   return (
     <>
       <div className="d-grid gap-4">
-        <Accordion defaultActiveKey="1" alwaysOpen>
-          {rewards.map((r, idx) => (
-            <>
-              <Accordion.Item eventKey={idx}>
-                <Accordion.Header>Reward {idx + 1}</Accordion.Header>
-                <AddLotteryRewardSetter
-                  idx={idx}
-                  reward={r}
-                  onDeleteReward={() => handleDeleteReward(idx)}
-                />
-              </Accordion.Item>
-            </>
-          ))}
+        <Accordion defaultActiveKey="1">
+          <Form
+            id="addLottery.rewards"
+            noValidate
+            validated={validated}
+            onSubmit={handleSubmit}
+          >
+            {rewards.map((r, idx) => (
+              <>
+                <Accordion.Item key={idx} eventKey={idx}>
+                  <Accordion.Header>Reward {idx + 1}</Accordion.Header>
+                  <AddLotteryRewardSetter
+                    idx={idx}
+                    isLast={rewards.length === 1 && idx === 0}
+                    reward={r}
+                    onDeleteReward={() => handleDeleteReward(idx)}
+                  />
+                </Accordion.Item>
+              </>
+            ))}
+          </Form>
         </Accordion>
-        <Button onClick={() => handleAddReward()}> Add Prize</Button>
+        <Button onClick={handleAddReward}>Add Prize</Button>
         <div className="d-grid gap-1 w-50 mx-auto">
           <Button
             onClick={() => navigate('../lottery-info')}
@@ -51,7 +99,9 @@ export function AddLotteryStep2() {
           >
             Back
           </Button>
-          <Button onClick={() => navigate('../missions')}>Next</Button>
+          <Button type="submit" form="addLottery.rewards">
+            Next
+          </Button>
         </div>
       </div>
     </>
--- src/components/add-lottery/general/AddLotteryRewardSetter.jsx
+++ src/components/add-lottery/general/AddLotteryRewardSetter.jsx
@@ -3,57 +3,83 @@ import { Button, Accordion, Form, InputGroup } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 function AddLotteryRewardSetter(props) {
-  const [rewardAmount, setRewardAmount] = useState(0);
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [rewardAmount, setRewardAmount] = useState(props.reward.quantity);
+  // const [selectedFile, setSelectedFile] = useState(null);
 
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    setSelectedFile(file);
-  };
+  // const handleFileChange = (event) => {
+  //   const file = event.target.files[0];
+  //   setSelectedFile(file);
+  // };
 
   return (
-    <Accordion.Body>
-      <Form>
-        <Form.Group controlId="formFileLg" className="mb-3">
+    <Accordion.Body className="d-grid gap-3">
+      {/* <Form.Group controlId="formFileLg" className="mb-3">
           <Form.Label>Reward Photo</Form.Label>
           <Form.Control type="file" size="lg" onChange={handleFileChange} />
           <div>{selectedFile && selectedFile.name}</div>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label> Reward Title</Form.Label>
-          <Form.Control type="text" placeholder="Reward Title..." />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-          <Form.Label> Description </Form.Label>
-          <Form.Control as="textarea" rows={6} />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label> Reward amount </Form.Label>
-          <InputGroup className="mb-3">
-            <Button
-              variant="outline-secondary"
-              data-type="minus"
-              disabled={rewardAmount === 0}
-              onClick={() => setRewardAmount(rewardAmount - 1)}
-            >
-              -
-            </Button>
-            <Form.Control
-              aria-label="Example text with two button addons"
-              type="int"
-              value={rewardAmount}
-            />
-            <Button
-              variant="outline-secondary"
-              data-type="plus"
-              onClick={() => setRewardAmount(rewardAmount + 1)}
-            >
-              +
-            </Button>
-          </InputGroup>
-        </Form.Group>
-      </Form>
-      <Button onClick={() => props.onDeleteReward()}>Delete</Button>
+        </Form.Group> */}
+      <Form.Group controlId={`addLottery.rewards.${props.idx}.imageURL`}>
+        <Form.Label>Reward Image URL</Form.Label>
+        <Form.Control
+          type="url"
+          name={`imageURL.${props.idx}`}
+          defaultValue={props.reward.imageURL}
+          placeholder="Enter reward banner URL..."
+          required
+        />
+      </Form.Group>
+      <Form.Group controlId={`addLottery.rewards.${props.idx}.title`}>
+        <Form.Label>Reward Title</Form.Label>
+        <Form.Control
+          type="text"
+          name={`title.${props.idx}`}
+          defaultValue={props.reward.title}
+          placeholder="Enter reward title..."
+          required
+        />
+      </Form.Group>
+      <Form.Group controlId={`addLottery.rewards.${props.idx}.description`}>
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          as="textarea"
+          name={`description.${props.idx}`}
+          defaultValue={props.reward.description}
+          rows={6}
+          placeholder="Enter reward description..."
+          required
+        />
+      </Form.Group>
+      <Form.Group controlId={`addLottery.rewards.${props.idx}.quantity`}>
+        <Form.Label>Reward Quantity</Form.Label>
+        <InputGroup className="mb-3">
+          <Button
+            variant="outline-secondary"
+            disabled={rewardAmount === 0}
+            onClick={() => setRewardAmount(rewardAmount - 1)}
+          >
+            <i className={`bi bi-dash-lg`}></i>
+          </Button>
+          <Form.Control
+            type="int"
+            name={`quantity.${props.idx}`}
+            value={rewardAmount}
+            required
+          />
+          <Button
+            variant="outline-secondary"
+            onClick={() => setRewardAmount(rewardAmount + 1)}
+          >
+            <i className={`bi bi-plus-lg`}></i>
+          </Button>
+        </InputGroup>
+      </Form.Group>
+      <Button
+        variant="danger"
+        onClick={() => props.onDeleteReward()}
+        disabled={props.isLast}
+      >
+        Delete
+      </Button>
     </Accordion.Body>
   );
 }
@@ -61,11 +87,12 @@ function AddLotteryRewardSetter(props) {
 AddLotteryRewardSetter.propTypes = {
   idx: PropTypes.number.isRequired,
   reward: PropTypes.shape({
-    photoPath: PropTypes.file,
-    Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    RewardAmount: PropTypes.number.isRequired,
+    imageURL: PropTypes.file,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
   }).isRequired,
+  isLast: PropTypes.bool.isRequired,
   onDeleteReward: PropTypes.func.isRequired,
 };
 
